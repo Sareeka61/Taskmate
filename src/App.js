adaa27094275131bd2594e46f148372b085d45e3
sareeka61 <ghimiresarika61@gmail.com>
2023-09-01 23:27:03 +0545

    todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {id: 1337, name: "Do react projects", completed: true}, {id: 1338, name: "Contemplate what's wrong with my life", completed: true}, {id: 1339, name: "Fold clothes of your bed and don't throw on chair & vice-versa", completed: false}
+  ]);
+
+  function handleDelete(id) {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span>{task.id} - {task.name} </span>
+            <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
