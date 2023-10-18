a2bd249fdd8b76b91a0bc21f8803a3076bab6b6b
sareeka61 <ghimiresarika61@gmail.com>
2023-10-18 21:49:56 +0545

    Added components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import { useState } from 'react';
+import { Header } from './components/Header';
+import { TaskList } from './components/TaskList';
 import './App.css';
 
 function App() {
 
-  const [tasks, setTasks] = useState([
-    {id: 1337, name: "Do react projects", completed: true}, {id: 1338, name: "Contemplate what's wrong with my life", completed: true}, {id: 1339, name: "Fold clothes of your bed and don't throw on chair & vice-versa", completed: false}
-  ]);
-  const [show, setShow] = useState(true);
-
-  function handleDelete(id) {
-    setTasks(tasks.filter(task => task.id !== id));
-  }
-
   return (
     <div className="App">
-      <h1>Task List</h1>
-      <ul>
-        <button className='trigger' onClick = {() => {setShow(!show)}}>Toggle</button>
-        { show && tasks.map((task) => (
-          <li key={task.id} className = {task.completed ? "completed" : "incompleted"}>
-            <span>{task.id} - {task.name} </span>
-            <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <Header />
+      <TaskList title="Random" subtitle="Test"/>
     </div>
   );
 }
---
+++ src/components/TaskList.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+
+export const TaskList = (props) => {
+     const [tasks, setTasks] = useState([
+    {id: 1337, name: "Do react projects", completed: true}, 
+    {id: 1338, name: "Contemplate what's wrong with my life", completed: true}, 
+    {id: 1339, name: "Fold clothes of your bed and don't throw on chair & vice-versa", completed: false}
+  ]);
+  const [show, setShow] = useState(true);
+
+  function handleDelete(id) {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  return (
+    <>
+      <h1>Task List {props.title} {props.subtitle}</h1>
+      <ul>
+        <button
+          className="trigger"
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          Toggle
+        </button>
+        {show &&
+          tasks.map((task) => (
+            <li
+              key={task.id}
+              className={task.completed ? "completed" : "incompleted"}
+            >
+              <span>
+                {task.id} - {task.name}{" "}
+              </span>
+              <button onClick={() => handleDelete(task.id)} className="delete">
+                Delete
+              </button>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
