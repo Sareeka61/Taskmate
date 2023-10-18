596d54b71a941884de9b241a52aa644a27b28a99
sareeka61 <ghimiresarika61@gmail.com>
2023-10-18 21:32:53 +0545

    Used trigger in task to show or hide

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [tasks, setTasks] = useState([
     {id: 1337, name: "Do react projects", completed: true}, {id: 1338, name: "Contemplate what's wrong with my life", completed: true}, {id: 1339, name: "Fold clothes of your bed and don't throw on chair & vice-versa", completed: false}
   ]);
+  const [show, setShow] = useState(true);
 
   function handleDelete(id) {
     setTasks(tasks.filter(task => task.id !== id));
@@ -15,8 +16,9 @@ function App() {
     <div className="App">
       <h1>Task List</h1>
       <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
+        <button className='trigger' onClick = {() => {setShow(!show)}}>Toggle</button>
+        { show && tasks.map((task) => (
+          <li key={task.id} className = {task.completed ? "completed" : "incompleted"}>
             <span>{task.id} - {task.name} </span>
             <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
           </li>
